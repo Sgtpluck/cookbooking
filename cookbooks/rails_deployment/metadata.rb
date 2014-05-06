37e0f1727f284c9caed4b4e5fd6e7fd3df7c3c8f
Davida Marion <davidamarion@gmail.com>
2014-05-06 10:13:51 -0700

    so many typos;

--- cookbooks/rails_deployment/metadata.rb
+++ cookbooks/rails_deployment/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email ''
 license          ''
 description      'Installs/Configures rails_deployment'
 long_description 'Installs/Configures rails_deployment'
-version          '0.1.0'
+version          '0.1.01'
 
 depends "application_ruby"
